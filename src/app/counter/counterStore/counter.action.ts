1cf2a13b54e78dfe1739ee57a329a36aa34f657a
amol-angularminds <amol@angularminds.in>
2023-09-27 14:23:58 +0530

    express-boilerplate

--- src/app/counter/counterStore/counter.action.ts
+++
@@ -1,9 +0,0 @@
-import { createAction, props } from "@ngrx/store";
-import { interfaceState } from "./counter.store"; 
-
-export const increment = createAction('increment');
-export const decrement = createAction('decrement');
-export const reset = createAction('reset');
-
-export const updateT = createAction('updateT',props<{title:string}>());
-export const updateCounterBy = createAction('updateCounterBy',props<{counter:number}>());
